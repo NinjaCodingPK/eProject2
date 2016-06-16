a64997b6ecea8bf23281acc218cf9a2b001a3eb9
wookie <kononenkoa995@ukr.net>
2016-06-16 22:47:44 +0300

    Model remastered.

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,11 +1,7 @@
 package model;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import entities.*;
 
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -14,13 +10,13 @@ import java.util.stream.Collectors;
  * Created by wookie on 6/12/16.
  */
 public class Model {
-    private Composite text;
+    private TextComposite text;
 
     public Model() {
 
     }
 
-    public Model(Composite text) {
+    public Model(TextComposite text) {
         this.text = text;
     }
 
@@ -32,16 +28,17 @@ public class Model {
         int max = 0;
         int currentMax;
 
-        for(Map.Entry<Integer, String> word : WordFactory.getWords().entrySet()) {
-            currentMax = 0;
-            for(Component paragraph : text.getComponents()) {
-                for(Component sentence : ((Composite)paragraph).getComponents()) {
-                    if(this.findRepeatsInSentence((Composite) sentence, word.getValue()))
-                        currentMax++;
-                }
+            for(TextComponent paragraph : text.getComponents()) {
+                for(TextComponent sentence : ((TextComposite)paragraph).getComponents()) {
+                    currentMax = 0;
+                    for(TextComponent word : ((TextComposite)sentence).getComponents()) {
+                        if(!(word instanceof Symbol))
+                            if(this.findRepeatsInSentence((TextComposite) sentence, word.getValue()))
+                                currentMax++;
+                    }
+                    if(currentMax > max)
+                        max = currentMax;
             }
-            if(currentMax > max)
-                max = currentMax;
         }
 
         return max;
@@ -49,17 +46,17 @@ public class Model {
 
     /**
      * Method checks if some word has repeats in sentence.
-     * @param sentence Composite class which defines a single sentence.
+     * @param sentence TextComposite class which defines a single sentence.
      * @param word a word, repeats of which the method will check.
      * @return true - if word has a repeats. False - if not.
      */
-    private boolean findRepeatsInSentence(Composite sentence, String word) {
+    private boolean findRepeatsInSentence(TextComposite sentence, String word) {
         return (sentence.getComponents().stream()
             .filter(f -> f.getValue().equals(word))
             .collect(Collectors.toList()).size() > 1);
     }
 
-    public void setText(Composite text) {
+    public void setText(TextComposite text) {
         this.text = text;
     }
 }
