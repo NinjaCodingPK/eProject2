f06e27386d4748be66ea52b31a1bd7b333683d3f
wookie <kononenkoa995@ukr.net>
2016-06-16 22:47:21 +0300

    Parser algorithm remastered.

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -3,18 +3,16 @@ package model;
 import constants.Constant;
 import entities.*;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by wookie on 6/12/16.
  */
 public class Parser {
-    private Composite text = new Text();
+    private TextComposite text = new TextComposite();
     private String strText;
     //private WordFactory wordFactory = new WordFactory();
 
@@ -25,17 +23,17 @@ public class Parser {
     /**
      * Method cast input string into entities.
      */
-    public void parse() {
-        List<Component> paragraphs = new ArrayList<>();
+    /*public void parse() {
+        List<TextComponent> paragraphs = new ArrayList<>();
 
         for(String paragraph : getRegexValues(strText, Constant.PARAGRAPH_REGEX)) {
 
-            List<Component> sentences = new ArrayList<>();
+            List<TextComponent> sentences = new ArrayList<>();
             for(String sentence : getRegexValues(paragraph, Constant.SENTENCE_REGEX)) {
-                List<Component> words = new ArrayList<>();
+                List<TextComponent> words = new ArrayList<>();
 
                 for(String word : getRegexValues(sentence, Constant.WORD_REGEX)) {
-                    words.add(new Word(WordFactory.getId(word)));
+                    words.add(WordFactory.getSymbol(word));
                 }
                 sentences.add(new Sentence(words));
             }
@@ -43,16 +41,44 @@ public class Parser {
         }
 
         this.text.setComponents(paragraphs);
+    }*/
+    public void parse() {
+        List<TextComponent> paragraphs = new ArrayList<>();
+
+        for(String paragraph : getRegexValues(strText, Constant.PARAGRAPH_REGEX)) {
+
+            List<TextComponent> sentences = new ArrayList<>();
+            for(String sentence : getRegexValues(paragraph, Constant.SENTENCE_REGEX)) {
+                 List<TextComponent> sentence_parts = new ArrayList<>();
+
+                for(String word : getRegexValues(sentence, Constant.WORD_REGEX)) {
+                    for(String ch : getRegexValues(word, Constant.SIGN_REGEX)) {
+                        sentence_parts.add(SymbolFactory.getSymbol(ch.charAt(0)));
+                    }
+
+                    List<TextComponent> sentence_word = new ArrayList<>();
+                    for(String ch : getRegexValues(word, Constant.LETTER_REGEX)) {
+                        sentence_word.add(SymbolFactory.getSymbol(ch.charAt(0)));
+                    }
+                    if (!sentence_word.isEmpty())
+                        sentence_parts.add(new TextComposite(sentence_word));
+                }
+                sentences.add(new TextComposite(sentence_parts));
+            }
+            paragraphs.add(new TextComposite(sentences));
+        }
+
+        this.text.setComponents(paragraphs);
     }
 
-//    private List<Component> parseStep(String text, String regex) throws IOException {
-//        List<Component> list = new ArrayList<>();
+//    private List<TextComponent> parseStep(String text, String regex) throws IOException {
+//        List<TextComponent> list = new ArrayList<>();
 //
 //        for(String s : getRegexValues(text, regex)) {
 //
 //        }
 //
-//        Composite temp = new Composite();
+//        TextComposite temp = new TextComposite();
 //        temp.setComponents(s);
 //        sentences.add(temp);
 //    }
@@ -65,23 +91,23 @@ public class Parser {
      */
     private List<String> getRegexValues(final String text, final String regex) {
         List<String> tagValues = new ArrayList<>();
-//        Pattern pattern = Pattern.compile(regex);
-//        Matcher matcher = pattern.matcher(text);
-//
-//        while (matcher.find()) {
-//            tagValues.add(matcher.group(1).trim());
-//        }
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
 
-        String[] words = text.trim().split(regex);
-        for(String s : words) {
-            if(!s.equals(""))
-                tagValues.add(s);
+        while (matcher.find()) {
+            tagValues.add(matcher.group());
         }
+
+//        String[] words = text.trim().split(regex);
+//        for(String s : words) {
+//            //if(!s.equals(""))
+//                tagValues.add(s);
+//        }
 
         return tagValues;
     }
 
-    public Composite getText() {
+    public TextComposite getText() {
         return text;
     }
 
