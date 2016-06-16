ae830303887140559f9f4251d2baed3adf9f4d33
wookie <kononenkoa995@ukr.net>
2016-06-17 01:03:39 +0300

    Parser algorithm refactored.

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -5,8 +5,10 @@ import entities.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by wookie on 6/12/16.
@@ -24,35 +26,65 @@ public class Parser {
     }
 
     /**
+     *
+     * @return function for forming symbols from some text.
+     */
+    private Function<String, TextComponent> getSymbolFunction() {
+        return ch -> SymbolFactory.getSymbol(ch.charAt(0));
+    }
+
+    /**
+     *
+     * @return function for forming sentences from some text.
+     */
+    private Function<String, TextComponent> getSentenceFunction() {
+        return sentence -> new TextComposite(parseSentence(sentence));
+    }
+
+    /**
+     *
+     * @return function for forming paragraphs from some text.
+     */
+    private Function<String, TextComponent> getParagraphFunction() {
+        return paragraph -> new TextComposite(parseStep(paragraph, Constant.SENTENCE_REGEX, getSentenceFunction()));
+    }
+
+    /**
+     * Method parse an input text in a way which provides input function.
+     * @param text text for parse.
+     * @param regex regular expression which will be used for parsing a text.
+     * @param f function which will be used for parsing an input text.
+     * @return List of parsed components.
+     */
+    private List<TextComponent> parseStep(String text, String regex, Function<String, TextComponent> f) {
+        return getRegexValues(text, regex).stream().
+                map(f).
+                collect(Collectors.toList());
+    }
+
+    /**
+     * Method form a "sentence" components with "word" components and "symbol" components.
+     * @param sentence sentence to parse.
+     * @return List of Components which include current sentence,
+     */
+    private List<TextComponent> parseSentence(String sentence) {
+        List<TextComponent> sentence_parts = new ArrayList<>();
+
+        for(String word : getRegexValues(sentence, Constant.WORD_REGEX)) {
+            if(!getRegexValues(word, Constant.SIGN_REGEX).isEmpty())
+                sentence_parts.addAll(parseStep(word, Constant.SIGN_REGEX, getSymbolFunction()));
+            else
+                sentence_parts.add(new TextComposite(parseStep(word, Constant.LETTER_REGEX, getSymbolFunction())));
+        }
+
+            return  sentence_parts;
+    }
+
+    /**
      * Method cast input string into entities.
      */
     public void parse() {
-        List<TextComponent> paragraphs = new ArrayList<>();
-
-        for(String paragraph : getRegexValues(strText, Constant.PARAGRAPH_REGEX)) {
-
-            List<TextComponent> sentences = new ArrayList<>();
-            for(String sentence : getRegexValues(paragraph, Constant.SENTENCE_REGEX)) {
-                 List<TextComponent> sentence_parts = new ArrayList<>();
-
-                for(String word : getRegexValues(sentence, Constant.WORD_REGEX)) {
-                    for(String ch : getRegexValues(word, Constant.SIGN_REGEX)) {
-                        sentence_parts.add(SymbolFactory.getSymbol(ch.charAt(0)));
-                    }
-
-                    List<TextComponent> sentence_word = new ArrayList<>();
-                    for(String ch : getRegexValues(word, Constant.LETTER_REGEX)) {
-                        sentence_word.add(SymbolFactory.getSymbol(ch.charAt(0)));
-                    }
-                    if (!sentence_word.isEmpty())
-                        sentence_parts.add(new TextComposite(sentence_word));
-                }
-                sentences.add(new TextComposite(sentence_parts));
-            }
-            paragraphs.add(new TextComposite(sentences));
-        }
-
-        this.text.setComponents(paragraphs);
+        this.text.setComponents(parseStep(strText, Constant.PARAGRAPH_REGEX, getParagraphFunction()));
     }
 
     /**
