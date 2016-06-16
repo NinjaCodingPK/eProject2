e36e288c99438057a8764afc1eb3be88e9c0c5d9
wookie <kononenkoa995@ukr.net>
2016-06-16 22:46:28 +0300

    Regex remastered.

--- src/constants/Constant.java
+++ src/constants/Constant.java
@@ -4,8 +4,11 @@ package constants;
  * Created by wookie on 6/12/16.
  */
 public interface Constant {
-    String WORD_REGEX = "[\\s|.|,|;|!|?|...]";   //"(.+?)[\\s|.|,|;|!|?|...]";
-    String PARAGRAPH_REGEX = "[\\n]";
-    String SENTENCE_REGEX = "[.|!|?|...]";
-
+    String REAPLECE_SYMBOLS = "(\\s{2,})|(\t)";
+    String CHANGE_TO_CHARACTER = " ";
+    String PARAGRAPH_REGEX = ".+((\u2029$)|(\\z)|(\\n))";
+    String SENTENCE_REGEX = "[^.!?\\s][^.!?]*(?:[.!?](?!['\"]?\\s|$)[^.!?]*)*[.!?]?[\\s]*+[\\n]*+";
+    String WORD_REGEX = "(\\w*[a-zA-Z]+\\w*)|([a-zA-Z_]+\\w*)|(\\p{Punct}+)|(\\p{Blank}+)|(\\n)";
+    String SIGN_REGEX = "(\\p{Punct}+)|(\\p{Blank}+)|(\\n)";
+    String LETTER_REGEX = "(\\w{1}+)";
 }
